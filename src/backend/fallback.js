6ba4943ebba559000809d1c3c635b77b05bfb799
Fazli Sapuan <fazli@sapuan.org>
2016-02-13 18:27:30 +0800

    Feature parity between glsl and fallback

--- src/backend/fallback.js
+++ src/backend/fallback.js
@@ -15,18 +15,55 @@
 
 		return temp;
 	}
+	
+	function getArgumentType(arg) {
+		if (Array.isArray(arg)) {
+			return 'Array';
+		} else if (typeof arg == "number") {
+			return 'Number';
+		} else if (arg instanceof GPUTexture) {
+			return 'Texture';
+		} else {
+			return 'Unknown';
+		}
+	}
 
 	/// JS fallback transformation, basically pure JS
 	///
 	/// @param inputFunction   The calling to perform the conversion
-	/// @param _threadDim      The thread dim array configuration
-	/// @param _blockDim       The block dim array configuration
-	/// @param paramObj        The parameter object
+	/// @param opt             The parameter object
 	///
 	/// @returns callable function if converted, else returns null
-	GPU.prototype._backendFallback = function(kernel, paramObj) {
+	GPU.prototype._backendFallback = function(kernel, opt) {
 		function ret() {
-			var threadDim = clone(paramObj.dimensions);
+			if (!opt.dimensions || opt.dimensions.length === 0) {
+				if (arguments.length != 1) {
+					throw "Auto dimensions only supported for kernels with only one input";
+				}
+				
+				var argType = getArgumentType(arguments[0]);
+				if (argType == "Array") {
+					opt.dimensions = getDimensions(argType);
+				} else if (argType == "Texture") {
+					opt.dimensions = arguments[0].dimensions;
+				} else {
+					throw "Auto dimensions not supported for input type: " + argType;
+				}
+			}
+			
+			var kernelArgs = [];
+			for (var i=0; i<arguments.length; i++) {
+				var argType = getArgumentType(arguments[i]);
+				if (argType == "Array" || argType == "Number") {
+					kernelArgs[i] = arguments[i];
+				} else if (argType == "Texture") {
+					kernelArgs[i] = arguments[i].toArray();
+				} else {
+					throw "Input type not supported: " + arguments[i];
+				}
+			}
+			
+			var threadDim = clone(opt.dimensions);
 			
 			while (threadDim.length < 3) {
 				threadDim.push(1);
@@ -52,14 +89,14 @@
 			for (ctx.thread.z=0; ctx.thread.z<threadDim[2]; ctx.thread.z++) {
 				for (ctx.thread.y=0; ctx.thread.y<threadDim[1]; ctx.thread.y++) {
 					for (ctx.thread.x=0; ctx.thread.x<threadDim[0]; ctx.thread.x++) {
-						ret[ctx.thread.z][ctx.thread.y][ctx.thread.x] = kernel.apply(ctx, arguments);
+						ret[ctx.thread.z][ctx.thread.y][ctx.thread.x] = kernel.apply(ctx, kernelArgs);
 					}
 				}
 			}
 					
-			if (paramObj.dimensions.length == 1) {
+			if (opt.dimensions.length == 1) {
 				ret = ret[0][0];
-			} else if (paramObj.dimensions.length == 2) {
+			} else if (opt.dimensions.length == 2) {
 				ret = ret[0];
 			}
 			
@@ -67,7 +104,17 @@
 		}
 		
 		ret.dimensions = function(dim) {
-			paramObj.dimensions = dim;
+			opt.dimensions = dim;
+			return ret;
+		};
+		
+		ret.wraparound = function() {
+			opt.wraparound = false;
+			return ret;
+		};
+		
+		ret.outputToTexture = function() {
+			opt.outputToTexture = false;
 			return ret;
 		};
 		
