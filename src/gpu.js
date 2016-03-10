79766170138eb2da2bc3a9d1bb62176dda7fefc2
Eugene Cheah <eugeneqin@gmail.com>
2016-03-10 23:10:08 +0800

    support_webgl() public function

--- src/gpu.js
+++ src/gpu.js
@@ -4,5 +4,22 @@
 /// Initialises the GPU.js library class which manages the WebGL context for the created functions.
 ///
 var GPU = (function() {
-	return gpu_core;
+	var GPU = gpu_core;
+	
+	///
+	/// Function: support_webgl
+	///
+	/// Return TRUE, if browser supports webgl AND canvas
+	///
+	/// Note: This function can also be called directly `GPU.support_webgl()`
+	///
+	/// Returns:
+	/// 	{Boolean} TRUE if browser supports webgl
+	///
+	function support_webgl() {
+		return gpu_utils.browserSupport_webgl();
+	}
+	GPU.prototype.support_webgl = support_webgl;
+	
+	return GPU;
 })();
