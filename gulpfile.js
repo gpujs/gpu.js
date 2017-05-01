f114c79126d376cd67674ef4c817cf4bb288e14b
Eugene Cheah <eugeneqin@gmail.com>
2017-05-02 01:30:04 +0800

    Precompiled GPU.js gulp file support

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,9 @@ var pkg = require('./package.json');
 
 /// Build the scripts
 gulp.task('build', function() {
-	return gulp.src([
+	return true &&
+		// GPU.JS core files
+		gulp.src([
 			'src/parser.js',
 			'src/utils.js',
 			'src/texture.js',
@@ -26,13 +28,32 @@ gulp.task('build', function() {
 		])
 		.pipe(concat('gpu.js'))
 		.pipe(header(fs.readFileSync('src/wrapper/prefix.js', 'utf8'), { pkg : pkg } ))
+		.pipe(gulp.dest('bin')) &&
+		// PrecompiledGPU.js sub files
+		gulp.src([
+			'src/utils.js',
+			'src/precompiled/PrecompiledGpu.js',
+			'src/precompiled/suffix.js'
+		])
+		.pipe(concat('PrecompiledGPU.js'))
+		.pipe(header(fs.readFileSync('src/precompiled/prefix.js', 'utf8'), { pkg : pkg } ))
 		.pipe(gulp.dest('bin'));
 });
 
 /// Minify the build script, after building it
 gulp.task('minify', ['build'], function() {
-	return gulp.src(['bin/gpu.js'])
+	return true &&
+		// GPU.JS core files
+		gulp.src(['bin/gpu.js'])
 		.pipe(rename('gpu.min.js'))
+		.pipe(uglify({
+			mangle: false,
+			preserveComments: "license"
+		}).on('error', gutil.log))
+		.pipe(gulp.dest('bin')) &&
+		// PrecompiledGPU.js 
+		gulp.src(['bin/PrecompiledGPU.js'])
+		.pipe(rename('PrecompiledGPU.min.js'))
 		.pipe(uglify({
 			mangle: false,
 			preserveComments: "license"
