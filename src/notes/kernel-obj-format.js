593d38256156a543890a53e38ab046f5912bcb5b
Eugene Cheah <eugeneqin@gmail.com>
2017-07-02 12:47:03 +0800

    Added notes JS file : for better linking in documentation engine

---
+++ src/notes/kernel-obj-format.js
@@ -0,0 +1,23 @@
+///
+/// File: kernel-obj-format
+///
+/// Class: KernelObjFormat
+///
+/// The kernelObj is a standard JS object that matches the following format.
+///
+/// This is generated via <exportKernelObj>
+///
+/// |---------------|---------------|---------------------------------------------------------------------------|
+/// | Name          | Value Type    | Description                                                               |
+/// |---------------|---------------|---------------------------------------------------------------------------|
+/// | isKernelObj   | <Boolean>     | Boolean true flag, that is compulsory in the kernel obj validation        |
+/// | opt           | <Object>      | { option object }                                                         |
+/// | jsFunctions   | <String>      | String representation of the JS functions                                 |
+/// | jsKernel      | <String>      | String representation of the JS kernel                                    |
+/// | glHeaders     | <String>      | Precompiled webgl prototype function headers                              |
+/// | glKernel      | <String>      | Precompiled webgl kernel functions                                        |
+/// | paramNames    | <Array>       | Array of strings, containing the kernel parameter names                   |
+/// | paramType     | <Array>       | Array of strings, containing the kernel parameter types, can only be      |
+/// |               |               | either "Array", "Int", "Float"                                            |
+/// |---------------|---------------|---------------------------------------------------------------------------|
+///
