f42d745ecf3faacf49a2b29a69c7b88d9b5044fd
Abhishek Soni <abhisheksoni2720@gmail.com>
2017-07-03 16:30:55 +0530

    gpu-core.js done

--- src/core/gpu-core.js
+++ src/core/gpu-core.js
@@ -1,13 +1,12 @@
 const UtilsCore = require("./utils-core");
 
 /**
- * @class GPUCore
- *
  * This is a minimalistic version of GPU.js used 
- * To run precompiled GPU.JS code
+ * to run precompiled GPU.JS code.
  *
- * This intentionally excludes the JS AST compiller : which is 400kb alone
+ * This intentionally excludes the JS AST compiller : which is 400kb alone/
  *
+ * @class GPUCore
  */
 module.exports = class GPUCore {
 
@@ -15,8 +14,9 @@ module.exports = class GPUCore {
 	 * @name validateKernelObj
 	 * @function
 	 * @static
+	 * @memberOf GPUCore
 	 *
-	 * Validates the KernelObj to comply with the defined format
+	 * @description Validates the KernelObj to comply with the defined format
 	 * Note that this does only a limited sanity check, and does not  
 	 * gurantee a full working validation.
 	 *
@@ -62,15 +62,15 @@ module.exports = class GPUCore {
 	 * @name loadKernelObj
 	 * @function
 	 * @static
+	 * @memberOf GPUCore
 	 *
-	 * Loads the precompilled kernel object. For GPUCore this is the ONLY way to create the kernel.
+	 * @description Loads the precompilled kernel object. For GPUCore this is the ONLY way to create the kernel.
 	 * To generate the kernelObj use <Kernel.exportKernelObj>
 	 *
 	 * Note that this function calls <validateKernelObj> internally, and throws an exception if it fails.
 	 *
-	 * See Also:
-	 * 	<GPUCore.validateKernelObj>,
-	 * 	<Kernel.exportKernelObj>
+	 * @see GPUCore.validateKernelObj
+	 * @see	Kernel.exportKernelObj
 	 *
 	 * @param {Object} kernelObj - The precompilled kernel object
 	 * @param {Object} inOpt - [Optional] the option overrides to use
