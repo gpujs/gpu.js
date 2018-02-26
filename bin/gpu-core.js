898ce6239e23208f2aac5691d30ab930f219f1df
Robert Plummer <robertleeplummerjr@gmail.com>
2018-02-25 19:12:00 -0500

    merge, and bump version

--- bin/gpu-core.js
+++ bin/gpu-core.js
@@ -4,8 +4,8 @@
  *
  * GPU Accelerated JavaScript
  *
- * @version 1.0.0
- * @date Mon Feb 12 2018 12:08:17 GMT-0500 (EST)
+ * @version 1.0.1
+ * @date Sun Feb 25 2018 19:11:17 GMT-0500 (EST)
  *
  * @license MIT
  * The MIT License
