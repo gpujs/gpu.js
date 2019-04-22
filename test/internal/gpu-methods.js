2c9bd8d6e1ab920ae85f4b423711d592450d9ff7
Robert Plummer <robertleeplummerjr@gmail.com>
2019-04-22 17:15:01 -0400

    fix: Test GPU.createKernelMap

---
+++ test/internal/gpu-methods.js
@@ -0,0 +1,97 @@
+const { assert, test, skip, module: describe, only } = require('qunit');
+const { GPU } = require('../../src');
+
+describe('internal: GPU methods');
+
+test('.createKernelMap() object map with settings', () => {
+  const gpu = new GPU();
+  let source = null;
+  let settings = null;
+  function bob() {}
+  function tom() {}
+  class MockKernel {
+    constructor(_source, _settings) {
+      source = _source;
+      settings = _settings;
+      this.context = 'context';
+      this.canvas = 'canvas';
+      this.subKernels = [];
+    }
+    addSubKernel(subKernel) {
+      this.subKernels.push(subKernel);
+    }
+  }
+  gpu.Kernel = MockKernel;
+  const subKernels = {
+    bobResult: bob,
+    tomResult: tom
+  };
+  const kernelSource = function() {};
+  const masterSettings = {};
+  const kernel = gpu.createKernelMap(subKernels, kernelSource, masterSettings);
+  assert.equal(source, kernelSource.toString());
+  assert.notEqual(settings, masterSettings);
+  assert.equal(gpu.canvas, 'canvas');
+  assert.equal(gpu.context, 'context');
+  assert.equal(settings.functions, gpu.functions);
+  assert.equal(settings.nativeFunctions, gpu.nativeFunctions);
+  assert.equal(settings.gpu, gpu);
+  assert.equal(settings.validate, true);
+  assert.deepEqual(kernel.subKernels, [
+    {
+      name: 'bob',
+      source: bob.toString(),
+      property: 'bobResult'
+    },
+    {
+      name: 'tom',
+      source: tom.toString(),
+      property: 'tomResult'
+    }
+  ]);
+});
+
+test('.createKernelMap() array map with settings', () => {
+  const gpu = new GPU();
+  let source = null;
+  let settings = null;
+  function bob() {}
+  function tom() {}
+  class MockKernel {
+    constructor(_source, _settings) {
+      source = _source;
+      settings = _settings;
+      this.context = 'context';
+      this.canvas = 'canvas';
+      this.subKernels = [];
+    }
+    addSubKernel(subKernel) {
+      this.subKernels.push(subKernel);
+    }
+  }
+  gpu.Kernel = MockKernel;
+  const subKernels = [bob, tom];
+  const kernelSource = function() {};
+  const masterSettings = {};
+  const kernel = gpu.createKernelMap(subKernels, kernelSource, masterSettings);
+  assert.equal(source, kernelSource.toString());
+  assert.notEqual(settings, masterSettings);
+  assert.equal(gpu.canvas, 'canvas');
+  assert.equal(gpu.context, 'context');
+  assert.equal(settings.functions, gpu.functions);
+  assert.equal(settings.nativeFunctions, gpu.nativeFunctions);
+  assert.equal(settings.gpu, gpu);
+  assert.equal(settings.validate, true);
+  assert.deepEqual(kernel.subKernels, [
+    {
+      name: 'bob',
+      source: bob.toString(),
+      property: 0
+    },
+    {
+      name: 'tom',
+      source: tom.toString(),
+      property: 1
+    }
+  ]);
+});
