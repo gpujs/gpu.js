d8ade9ba1cbba604801c778495c6c0e50f4e42f0
17097231932 <17097231932@163.com>
2023-01-29 12:15:25 +0800

    extract the test builder to standalone file then we can remove gulp and other unused dependcies

---
+++ scripts/build-tests.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { readDirDeepSync } = require('read-dir-deep');
+
+const folder = 'test';
+const testFile = 'all.html';
+
+try {
+  fs.unlinkSync(`${folder}/${testFile}`);
+} catch (e) {}
+
+const rootPath = path.resolve(process.cwd(), folder);
+const warning = '<!-- the following list of javascript files is built automatically -->\n  ';
+const tags = readDirDeepSync(rootPath, {
+    patterns: [
+      '**/*.js'
+    ],
+    ignore: [
+      '*.js'
+    ]
+  })
+  .map(file => './' + file.replace(/^test\//, ''))
+  .map(file => `<script type="module" src="${file}"></script>`)
+  .join('\n  ');
+
+const template = fs.readFileSync(`${folder}/all-template.html`, 'utf-8')
+const text = template.replace('{{test-files}}', warning + tags)
+
+fs.writeFileSync(`${folder}/${testFile}`, text, 'utf-8')
